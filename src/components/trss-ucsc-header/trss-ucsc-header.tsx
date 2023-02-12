74168eaf359f402b42bd85f43405ab6c6193b718
Rob Knight <raknight@ucsc.edu>
2023-02-12 12:23:44 -0800

    fix(Component): 🐛 Fix mobile display of trss-ucsc-header

--- src/components/trss-ucsc-header/trss-ucsc-header.tsx
+++ src/components/trss-ucsc-header/trss-ucsc-header.tsx
@@ -7,9 +7,9 @@ import { Component, Prop, h } from '@stencil/core';
 })
 export class TrssUcscHeader {
 
- /**
-   * Whether or not to display the UCSC logo
-   */
+  /**
+    * Whether or not to display the UCSC logo
+    */
   @Prop() useLogo: boolean;
 
   /**
@@ -24,34 +24,34 @@ export class TrssUcscHeader {
 
 
   private parentClasses(): string {
-    return 'ucsc-header has-logo ucsc-row__inner';
+    return 'has-logo ucsc-row__inner';
   }
 
   render() {
     return (
-      <div class="ucsc-row__full two-blues">
+      <div class="ucsc-row__full trss-ucsc-header">
         <slot />
-        <div class={this.useLogo ? this.parentClasses() : 'ucsc-header'}>
+        <div class={this.useLogo ? this.parentClasses() : 'ucsc-row__inner'}>
           {this.useLogo ? (
-              <div class="ucsc-header__left">
-                <trss-logo width="114" display='light' with-animation />
-              </div>
-            ) : null}
-          {/* <input type="checkbox" id="ucsc-header__toggle" /> */}
-          <div class="ucsc-header__right" id="ucsc-header__show">
-            <ul class="ucsc-header__navigation">
+            <div class="trss-ucsc-header__left">
+              <trss-logo width="114" display='light' with-animation />
+            </div>
+          ) : null}
+          {/* <input type="checkbox" id="trss-ucsc-header__toggle" /> */}
+          <div class="trss-ucsc-header__right" id="trss-ucsc-header__show">
+            <ul class="trss-ucsc-header__navigation">
               <li><a href="https://my.ucsc.edu" title="The student portal">MyUCSC</a></li>
               <li><a href="https://www.ucsc.edu/tools/people.html" title="Campus directory">People</a></li>
               <li><a href="https://www.ucsc.edu/tools/calendars.html" title="Upcoming events, academic, and administrative calendars">Calendars</a></li>
               <li><a href="https://www.ucsc.edu/visit/maps-directions.html">Maps</a></li>
               <li><a href="https://www.ucsc.edu/tools/azindex.html" title="A to Z index of UCSC websites">A-Z Index</a></li>
             </ul>
-            <div class="ucsc-header__search" role="search">
+            <div class="trss-ucsc-header__search" role="search">
               <form role="search" method="get" action={this.searchAction} id="cse-search-box">
                 <div>
                   <label class="hide" htmlFor={this.searchQuery}>Search</label>
-                  <input class="query" id={this.searchQuery} name={this.searchQuery} placeholder="Search" type="text" value=""/>
-                  <input type="submit" value="Search"/>
+                  <input class="query" id={this.searchQuery} name={this.searchQuery} placeholder="Search" type="text" value="" />
+                  <input type="submit" value="Search" />
                 </div>
               </form>
             </div>
