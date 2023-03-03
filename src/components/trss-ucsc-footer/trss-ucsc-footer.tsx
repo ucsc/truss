df03f4f5d35e1ddbd77a4d288461eb7a1c38d39a
Rob Knight <raknight@ucsc.edu>
2023-03-03 11:07:40 -0800

    refactor(Component): 🎨 Clean up footer component folder

--- src/components/trss-ucsc-footer/trss-ucsc-footer.tsx
+++ src/components/trss-ucsc-footer/trss-ucsc-footer.tsx
@@ -12,26 +12,26 @@ export class TrssUcscFooter {
   render() {
     return (
 
-    <div class="trss-ucsc-footer__outer">
-      <div class="trss-ucsc-footer" role="contentinfo">
-        <div class="trss-ucsc-footer__inner">
+      <div class="trss-ucsc-footer__outer">
+        <div class="trss-ucsc-footer" role="contentinfo">
+          <div class="trss-ucsc-footer__inner">
 
-        <div class="trss-ucsc-footer__inner-left">
-          <trss-logo width="130" display='light' with-animation />
-        </div>
+            <div class="trss-ucsc-footer__inner-left">
+              <trss-logo width="130" display='light' with-animation />
+            </div>
 
-        <div class="trss-ucsc-footer__inner-right">
-          <ul>
-            <li><a href="https://academicaffairs.ucsc.edu/accreditation/">Accreditation</a></li>
-            <li><a href="https://www.ucsc.edu/about/employment.html">Employment</a></li>
-            <li><a href="https://its.ucsc.edu/terms/">Privacy</a></li>
-            <li>&copy;2023 UC Regents</li>
-          </ul>
-        </div>
+            <div class="trss-ucsc-footer__inner-right">
+              <ul>
+                <li><a href="https://academicaffairs.ucsc.edu/accreditation/">Accreditation</a></li>
+                <li><a href="https://www.ucsc.edu/about/employment.html">Employment</a></li>
+                <li><a href="https://its.ucsc.edu/terms/">Privacy</a></li>
+                <li>&copy;2023 UC Regents</li>
+              </ul>
+            </div>
 
+          </div>
         </div>
       </div>
-    </div>
     );
   }
 
