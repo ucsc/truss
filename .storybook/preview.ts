9f873edc222d230b3b8045f9f8889f80df381da1
Rob Knight <raknight@ucsc.edu>
2026-04-16 14:14:51 -0700

    chore: add storybook preview with custom-element registration and global styles

---
+++ .storybook/preview.ts
@@ -0,0 +1,18 @@
+import type { Preview } from '@storybook/web-components';
+import { defineCustomElements } from '../loader';
+import '../src/globals/ucsc-trss.scss';
+
+defineCustomElements();
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
